4e898b0489dbecfacf21ec896bed7aa0f926d007
CTichy <carlos.tichy@gmail.com>
2024-11-26 11:47:06 +0100

    Fixi issue with images for testing.

--- test_app.py
+++ test_app.py
@@ -21,14 +21,14 @@ class TestPostDatabase(unittest.TestCase):
         Test adding a post to the database and retrieving it.
         """
         # Add a new post
-        self.db.add_post(image_path="image.jpg", text="Test post", user="user1")
+        self.db.add_post(image_path="images\\chicken.jpeg", text="Test post", user="user1")
 
         # Retrieve the latest post
         latest_post = self.db.get_latest_post()
 
         # Assert that the post was added and data matches
         self.assertIsNotNone(latest_post)
-        self.assertEqual(latest_post.image, "image.jpg")
+        self.assertEqual(latest_post.image, "chicken.jpeg")
         self.assertEqual(latest_post.text, "Test post")
         self.assertEqual(latest_post.user, "user1")
 
@@ -45,15 +45,15 @@ class TestPostDatabase(unittest.TestCase):
         Test adding multiple posts and retrieving the latest one.
         """
         # Add multiple posts
-        self.db.add_post(image_path="image1.jpg", text="Post 1", user="user1")
-        self.db.add_post(image_path="image2.jpg", text="Post 2", user="user2")
+        self.db.add_post(image_path="images\\donkey.jpeg", text="Post 1", user="user1")
+        self.db.add_post(image_path="images\\pig.jpeg", text="Post 2", user="user2")
 
         # Retrieve the latest post
         latest_post = self.db.get_latest_post()
 
         # Assert that the latest post matches the last one added
         self.assertIsNotNone(latest_post)
-        self.assertEqual(latest_post.image, "image2.jpg")
+        self.assertEqual(latest_post.image, "pig.jpeg")
         self.assertEqual(latest_post.text, "Post 2")
         self.assertEqual(latest_post.user, "user2")
 
@@ -62,7 +62,7 @@ class TestPostDatabase(unittest.TestCase):
         Test wiping the database by deleting all entries.
         """
         # Add a post
-        self.db.add_post(image_path="image.jpg", text="To be deleted", user="user1")
+        self.db.add_post(image_path="images\\chicken.jpeg", text="To be deleted", user="user1")
 
         # Wipe the database (simulate by clearing the table)
         with self.db.conn:
